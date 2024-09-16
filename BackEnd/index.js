3cb80fade0fce5dcb19ade0a2349e91d43318a76
Shubham <shubhamsharma02303@gmail.com>
2024-09-16 23:27:28 +0530

    created user model,controller and routes for login,logout,register, and updateprofile

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -1,12 +1,28 @@
-import express from "express"
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./utils/db.js";
+import userRoute from "./routes/user.route.js";
 
-const app = express()
-const port = 3000
+const app = express();
+dotenv.config();
+const PORT = process.env.PORT || 4000;
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-  })
-  
-  app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-  })
+// middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+// routes
+app.use("/api/user", userRoute);
+
+app.listen(PORT, () => {
+  connectDB();
+  console.log(`Example app listening on port ${PORT}`);
+});
